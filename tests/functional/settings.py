761ed5540a53cfd16789a34aa907b63643ac0ccf
arthur hodosh <arthurhodosh2011@gmail.com>
2022-06-25 15:17:24 +0300

    fix settings

--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -3,5 +3,5 @@ from pydantic import BaseSettings, Field
 
 class TestSettings(BaseSettings):
     es_host: str = Field('http://127.0.0.1:9200', env='ELASTIC_HOST')
-    redis_host: str = Field('http://127.0.0.1:6379', env='REDIS_HOST')
+    redis_host: str = Field('redis://127.0.0.1:6379', env='REDIS_HOST')
     api_host: str = Field('http://127.0.0.1:8000', env='API_HOST')
