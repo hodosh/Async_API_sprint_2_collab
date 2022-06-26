6e3c15685f949acca76115982890d0d96cc8f32b
arthur hodosh <arthurhodosh2011@gmail.com>
2022-06-27 00:07:47 +0300

    ass tests for searching films

--- tests/functional/src/test_film.py
+++ tests/functional/src/test_film.py
@@ -45,3 +45,25 @@ class TestFilm:
         assert len(response.body) == 8
 
         assert response.body == results.film_by_id_result
+
+    @pytest.mark.asyncio
+    async def test_get_not_existing_film(self, es_client, make_get_request):
+        # Выполнение запроса
+        response = await make_get_request(f'/films/FAKE')
+
+        # Проверка результата
+        assert response.status == 404
+        assert response.body == {'detail': 'film not found'}
+
+    @pytest.mark.asyncio
+    async def test_film_search_base(self, es_client, make_get_request):
+        # Выполнение запроса
+        response = await make_get_request(f'/films/')
+
+        # Проверка результата
+        assert response.status == 200
+        # Проверяем, что возвращается список фильмов
+        assert isinstance(response.body['items'], list)
+        assert len(response.body['items']) > 0
+        # Проверяем ключи возвращенных фильмов
+        assert list(response.body['items'][0].keys()) == ['uuid', 'title', 'imdb_rating']
