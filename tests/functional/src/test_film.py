3094a1915bc3fbc2367ddbf873a83a4d807cff08
arthur hodosh <arthurhodosh2011@gmail.com>
2022-06-28 21:53:08 +0300

    fix tests

--- tests/functional/src/test_film.py
+++ tests/functional/src/test_film.py
@@ -1,11 +1,7 @@
-import json
-
 import pytest
-from elasticsearch import Elasticsearch
 
-import testdata.films as test_data
-from settings import settings
 import testdata.results.films as results
+from utils.common import prepare_es_actions
 
 
 class TestFilm:
@@ -13,31 +9,18 @@ class TestFilm:
     def setup_class(cls):
         # метод, в котором можно определить действия ДО выполнения тестов данного класса
         # например, тут будут загружаться общие для всех тестов тестовые данные
-        # PS не работает асинхронно, поэтому такая конструкция
-        data = ''
-        with Elasticsearch(hosts=f'{settings.es_host.rstrip("/")}:{settings.es_port}') as client:
-            for film in test_data.es_films_list:
-                film_id = film.get('id')
-                data += f'{{ "index" : {{ "_index" : "movies", "_id" : "{film_id}" }} }}\n{json.dumps(film)}\n'
-            client.bulk(data)
+        prepare_es_actions('prepare_test_data.json')
 
     @classmethod
     def teardown_class(cls):
         # метод, в котором можно определить действия ПОСЛЕ выполнения тестов данного класса
         # например, тут будут удаляться общие для всех тестов тестовые данные (чистим за собой мусор)
-        # PS не работает асинхронно, поэтому такая конструкция
-        data = ''
-        with Elasticsearch(hosts=f'{settings.es_host.rstrip("/")}:{settings.es_port}') as client:
-            for film in test_data.es_films_list:
-                film_id = film.get('id')
-                data += f'{{ "delete" : {{ "_index" : "movies", "_id" : "{film_id}" }} }}\n'
-            client.bulk(data)
+        prepare_es_actions('remove_test_data.json')
 
     @pytest.mark.asyncio
     async def test_get_film_by_id(self, es_client, make_get_request):
         # Выполнение запроса
-        film = test_data.es_films_list[0]
-        film_id = film.get('id')
+        film_id = '111111-111111-111111-111111'
         response = await make_get_request(f'/films/{film_id}')
 
         # Проверка результата
