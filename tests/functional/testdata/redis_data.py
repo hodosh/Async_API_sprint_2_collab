352fcafaf0c1b3b331130ef64867cd4b1a9dd214
arthur hodosh <arthurhodosh2011@gmail.com>
2022-07-03 19:34:55 +0300

    put redis test for person

--- tests/functional/testdata/redis_data.py
+++ tests/functional/testdata/redis_data.py
@@ -21,3 +21,21 @@ film_data = {
         'writers': [],
     },
 }
+
+person_data = {
+    'key': '222222-000000-000000-redis',
+    'data': {
+        'id': '222222-000000-000000-redis',
+        'full_name': 'George Lucas Jr II',
+        'film_roles': [
+            {
+                'id': 'e5a21648-59b1-4672-ac3b-867bcd64b6ea',
+                'name': 'writer',
+            },
+            {
+                'id': 'c8f57f93-b02a-40d4-ba55-9600cceddd7e',
+                'name': 'director',
+            },
+        ],
+    },
+}
