a6e1c07012e2737e749791ab2c22fde35151b23a
Ilia Suvorkin <ilyafx@gmail.com>
2022-07-03 03:04:43 +0300

    Fix Redis cache

--- app/src/models/models.py
+++ app/src/models/models.py
@@ -8,15 +8,6 @@ from pydantic import BaseModel
 def orjson_dumps(v: Any, *, default: Any) -> str:
     return orjson.dumps(v, default=default).decode()
 
-
-def init_from(new_class, ref_obj):
-    prop = [i for i in ref_obj.__dict__.keys() if i[:1] != '_']
-    new_args = {}
-    for k in prop:
-        new_args[k] = getattr(ref_obj, k)
-    return new_class(**new_args)
-
-
 class ORJSONModel(BaseModel):
     class Config:
         json_loads = orjson.loads
