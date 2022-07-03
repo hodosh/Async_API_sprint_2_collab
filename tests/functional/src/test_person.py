7e16a08975ec7025c2b18a0d28b6b6b84f7332a3
arthur hodosh <arthurhodosh2011@gmail.com>
2022-07-03 17:55:40 +0300

    first person's test

--- tests/functional/src/test_person.py
+++ tests/functional/src/test_person.py
@@ -12,18 +12,22 @@ class TestPerson:
     def setup_class(cls):
         # метод, в котором можно определить действия ДО выполнения тестов данного класса
         # например, тут будут создаваться тестовый индекс и загружаться общие для всех тестов тестовые данные
-        # todo описать индекс в testdata/persons/schema.json
-        # todo описать данные в testdata/persons/prepare_test_data.json
         pre_tests_actions(index_name=INDEX_NAME, data_path_name=DATA_PATH_NAME)
 
     @classmethod
     def teardown_class(cls):
         # метод, в котором можно определить действия ПОСЛЕ выполнения тестов данного класса
         # например, тут будут удаляться общие для всех тестов тестовые данные (чистим за собой мусор)
-        # todo описать данные в testdata/persons/remove_test_data.json
         post_tests_actions(index_name=INDEX_NAME, data_path_name=DATA_PATH_NAME)
 
     @pytest.mark.asyncio
-    async def test_get_person_by_id_success(self, es_client, make_get_request):
-        # todo крупные результаты тестов лучше описывать тут testdata/persons/results.py
-        pass
+    async def test_get_person_by_id_success(self, make_get_request):
+        # Выполнение запроса
+        person_id = '222222-000000-000000-000000'
+        response = await make_get_request(f'/persons/{person_id}')
+
+        # Проверка результата
+        assert response.status == 200
+        assert response.body
+
+        assert response.body == results.person_by_id_result
